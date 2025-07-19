cff87f469b38734f115d76008322c7b02200a2d2
erika <evillaq1@est.ups.edu.ec>
2025-07-19 18:50:30 -0500

    Remove estudiante feature components and related routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'estudiantes',
-    loadChildren: () => import('../app/features/estudiantes/routes').then(m => m.estudianteRoutes)
-  },
-//   {
-//     path: 'grupos',
-//     loadChildren: () => import('./grupos/routes').then(m => m.grupoRoutes)
-//   },
-//   {
-//     path: 'horarios',
-//     loadChildren: () => import('./horarios/routes').then(m => m.horarioRoutes)
-//   },
-//   {
-//     path: 'matriculas',
-//     loadChildren: () => import('./matriculas/routes').then(m => m.matriculaRoutes)
-//   },
-//   { path: '', redirectTo: '/estudiantes', pathMatch: 'full' },
+ 
 ];
--- src/app/features/estudiantes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { EstudianteListComponent } from '../estudiantes/estudiantes-list.component';
-import { EstudianteFormComponent } from '../estudiantes/estudiantes-form.component';
-
-export const estudianteRoutes: Routes = [
-  { path: '', component: EstudianteListComponent },
-  { path: 'nuevo', component: EstudianteFormComponent },
-  { path: 'editar/:id', component: EstudianteFormComponent }
-];
