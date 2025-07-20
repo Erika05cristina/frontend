84e0c9e5eb9eea41c08332ac1abc69e482d806f7
erika <evillaq1@est.ups.edu.ec>
2025-07-19 23:11:42 -0500

    agregando rutas para calificaciones, clases, comprobantes de venta, cuentas contables y docentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from '@angular/router';
+import { Asignaturas } from './components/asignaturas/asignaturas';
+import { Calificaciones } from './components/calificaciones/calificaciones';
+import { Clases } from './components/clases/clases';
+import { CuentasContables } from './components/cuentas-contables/cuentas-contables';
+import { ComprobantesVenta } from './service/comprobantes-venta';
+import { Docentes } from './components/docentes/docentes';
+import { DiariosCaja } from './components/diarios-caja/diarios-caja';
 import { EspaciosFisicos } from './components/espacios-fisicos/espacios-fisicos';
 import { Estudiantes } from './components/estudiantes/estudiantes';
 import { Grupos } from './components/grupos/grupos';
 import { Horarios } from './components/horarios/horarios';
 import { Matriculas } from './components/matriculas/matriculas';
 import { ParametrosGenerales } from './components/parametros-generales/parametros-generales';
-import { Asignaturas } from './components/asignaturas/asignaturas';
 
 export const routes: Routes = [
   { path: '',  redirectTo: 'estudiantes', pathMatch: 'full' },
   { path: 'asignaturas', component: Asignaturas },
+  { path: 'calificaciones', component: Calificaciones },
+  { path: 'clases', component: Clases },
+  { path: 'comprobantes-venta', component: ComprobantesVenta },
+  { path: 'cuentas-contables', component: CuentasContables },
+  { path: 'diarios-caja', component: DiariosCaja },
+  { path: 'docentes', component: Docentes },
   { path: 'espacios-fisicos', component: EspaciosFisicos },
   { path: 'estudiantes', component: Estudiantes },
   { path: 'grupos', component: Grupos },
