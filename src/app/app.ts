75bae55a9c2c68c48fcb86c2f6fbc1665a5bd40c
r-ART26 <robertortups@gmail.com>
2025-07-22 13:48:32 -0500

    adicion de footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Sidebar],
+  imports: [RouterOutlet, Sidebar, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
